dbdef19a18dd722e11dc9e4eab05fa7ce6879565
Dima Shibun <shibun2006@gmail.com>
2025-04-17 08:43:38 +0300

    добавление дополнительных зависимостей, а также необходимые тесты

--- src/main/java/com/example/demo/controller/controller.java
+++ src/main/java/com/example/demo/controller/controller.java
@@ -2,13 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.model.model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
-public class controller {
-    @GetMapping("/greet")
-    public model greet(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return new model(String.format("Hello, %s!", name));
-    }
-}
+@RestController
+   public class controller {
+
+       @GetMapping("/hello")
+       public String hello() {
+           return "Hello, World!";
+       }
+   }
